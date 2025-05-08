89e4c0f1b100472c128064d1968be6e94fd37ccc
bkuehnis <62989491+bkuehnis@users.noreply.github.com>
2025-05-08 12:38:22 +0200

    change advisor

--- uebung11/OpenAiConfig.java
+++ uebung11/OpenAiConfig.java
@@ -3,7 +3,7 @@ package ch.zhaw.freelancer4u.config;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
-import org.springframework.ai.chat.memory.InMemoryChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -14,9 +14,9 @@ public class OpenAiConfig {
 
     @Autowired
     OpenAiChatModel chatModel;
-    
+
     @Bean
     public ChatClient chatClient() {
-        return ChatClient.builder(chatModel).defaultAdvisors(new MessageChatMemoryAdvisor(new InMemoryChatMemory()), new SimpleLoggerAdvisor()).build();
+        return ChatClient.builder(chatModel).defaultAdvisors(new MessageChatMemoryAdvisor(MessageWindowChatMemory.builder().build()), new SimpleLoggerAdvisor()).build();
     }
 }
